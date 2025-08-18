11349cfb3c8e064ef3669947697665cd7e1c326c
theoephraim <theo@dmno.dev>
2025-08-18 15:49:08 +0000

    varlock@0.0.9

--- Formula/varlock.rb
+++ Formula/varlock.rb
@@ -8,24 +8,24 @@ class Varlock < Formula
   on_macos do
     on_intel do
       url "https://github.com/dmno-dev/varlock/releases/download/varlock@#{version}/varlock-macos-x64.tar.gz"
-      sha256 "a42d2f7b398aab0d1da88fed188b7743b0876ff06d007fa50931abe5d3627f17"
+      sha256 "f6c5cfdb29f3aeab0b7c34ee618c986a52718e579eb2ff7c3b4c4ea3b4cf23e9"
     end
 
     on_arm do
       url "https://github.com/dmno-dev/varlock/releases/download/varlock@#{version}/varlock-macos-arm64.tar.gz"
-      sha256 "ea5e291e91675e1ceca6361f3ab873d193f9a0f69dffe49210f3063d01920f5d"
+      sha256 "bf1ac55907c681d18ddb84150054f0513cd45db54c472679dc5d49d60ef91853"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dmno-dev/varlock/releases/download/varlock@#{version}/varlock-linux-x64.tar.gz"
-      sha256 "3426b4a8535db242b61aebfe9f2506b2d7ee53ccd730180cdc1ff4a79333fc7e"
+      sha256 "f8ecc0e47fabf7a1c67a7dac0797d389404c3fb3fea709ee98e127be77294c1b"
     end
 
     on_arm do
       url "https://github.com/dmno-dev/varlock/releases/download/varlock@#{version}/varlock-linux-arm64.tar.gz"
-      sha256 "644b56282b82861a9156066f54dff759fe83bd3d66a16c7919a4e86291e85eb5"
+      sha256 "8c0113e8782f4d0325e202a1e9f68dba4bf56e0bb8d4742ef31a43982900b436"
     end
   end
 
